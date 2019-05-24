2124f35a5dac574a30215be06af42bea8adb7915
Paulo Eduardo Campos Afonso <pauloeduardocamposafonso@Akhenaton.local>
2019-05-24 18:07:59 -0300

    Mapa 1 - Correção das imagens das legendas

--- src/components/mapbox/legends/components/RAISGPressoes2018_geral_escala2.js
+++ src/components/mapbox/legends/components/RAISGPressoes2018_geral_escala2.js
@@ -52,31 +52,31 @@ class RAISGPressoes2018_geral_escala2 extends Component {
                 <span className={this.state.arrowClass} onClick={this.toggle}></span>
             </label>
             <Collapse isOpened={this.state.collapse}>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px',paddingTop:'10px'}}><img src={require('images/legend-icons/ico_square_0.gif')} alt='ico_circle-11_active2' title='ico_circle-11_active2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_0" defaultMessage="active">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px',paddingTop:'10px'}}><img src={require('images/legend-icons/ico_square_0.png')} alt='ico_circle-11_active2' title='ico_circle-11_active2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_0" defaultMessage="active">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_1.gif')} alt='ico_circle-11_active2' title='ico_circle-11_active2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_1" defaultMessage="active">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_1.png')} alt='ico_circle-11_active2' title='ico_circle-11_active2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_1" defaultMessage="active">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_2.gif')} alt='ico_circle-11_inactive2' title='ico_circle-11_inactive2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_2" defaultMessage="inactive">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_2.png')} alt='ico_circle-11_inactive2' title='ico_circle-11_inactive2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_2" defaultMessage="inactive">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_3.gif')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_3" defaultMessage="no information">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_3.png')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_3" defaultMessage="no information">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_4.gif')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_4" defaultMessage="no information">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_4.png')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_4" defaultMessage="no information">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_5.gif')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_5" defaultMessage="no information">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_5.png')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_5" defaultMessage="no information">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
-                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_6.gif')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_6" defaultMessage="no information">
+                <div className='mapbox_control-panel_subtitle'><span style={{'width':'15px',height:'15px'}}><img src={require('images/legend-icons/ico_square_6.png')} alt='ico_circle-11_noinfo2' title='ico_circle-11_noinfo2' style={{'margin':0,'padding':0}} /></span><FormattedMessage id="legend1.legend2_1_6" defaultMessage="no information">
                     {(txt) => (txt)}
                 </FormattedMessage>
                 </div>
